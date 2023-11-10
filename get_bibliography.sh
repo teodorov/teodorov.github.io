c03347f6a389fe94644acd58fb7533d1fbcf5025
Ciprian Teodorov <ciprian.teodorov@gmail.com>
2023-11-10 17:29:17 +0100

    add fi ligat

--- get_bibliography.sh
+++ get_bibliography.sh
@@ -1,2 +1,3 @@
 curl "https://api.archives-ouvertes.fr/search/?q=authIdHal_i:9871&wt=bibtex&rows=1000" > _bibliography/papers.bib
-sed -i 's/forall$min/forall$ min/g' _bibliography/papers.bib
+gsed -i 's/forall$min/forall$ min/g' _bibliography/papers.bib
+gsed -i 's/Veri cation/Verification/g' _bibliography/papers.bib
